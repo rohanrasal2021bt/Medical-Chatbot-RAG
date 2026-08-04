cb22c1d18c6f03c4c230f3d242b986a1d2a32733
rohanrasal2021bt <rohanrasal2021bt@gmail.com>
2026-08-04 22:27:53 +0530

    Replace repository contents

---
+++ app.py
@@ -0,0 +1,83 @@
+from flask import Flask, render_template, request
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_groq import ChatGroq
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+
+# Hugging Face Embeddings
+embeddings = download_hugging_face_embeddings()
+
+# Pinecone
+index_name = "medibot"
+
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+
+retriever = docsearch.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 3}
+)
+
+# Groq LLM
+chatModel = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    api_key=GROQ_API_KEY,
+    temperature=0
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(
+    chatModel,
+    prompt
+)
+
+rag_chain = create_retrieval_chain(
+    retriever,
+    question_answer_chain
+)
+
+@app.route("/")
+def index():
+    return render_template("chat.html")
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+
+    response = rag_chain.invoke(
+        {"input": msg}
+    )
+
+    print(response["answer"])
+
+    return response["answer"]
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=8080,
+        debug=True
+    )
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Rohan",
+    author_email="rohan_xxxx@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
